efc3a7fce3669f9b2aea651c913fca97ab49cdcb
kizito917 <johnkingsley917@gmail.com>
2024-01-16 12:07:07 +0100

    Changes made in codeflaks js file.

--- src/codeflask.js
+++ src/codeflask.js
@@ -149,6 +149,7 @@
     * @param {KeyboardEvent} e - key up event
     */
    onKeyUp(e) {
+    console.log('yeeeeee');
      if (e.code !== 'Backspace' && e.code !== 'Delete') {
        return;
      }
